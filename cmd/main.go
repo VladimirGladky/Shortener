896f734f6b488103cf65ee58cae6bda5e0648e75
VladimirGladky <vovagl23@mail.ru>
2026-03-07 04:58:30 +0300

    feat: add app pkg for run project

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"Shortener/internal/app"
+	"Shortener/pkg/logger"
+	"context"
 
+	"github.com/wb-go/wbf/config"
+)
+
+func main() {
+	cfg := config.New()
+	cfg.EnableEnv("")
+	err := cfg.LoadEnvFiles(".env")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx, err := logger.New(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	newApp := app.NewApp(cfg, ctx)
+	newApp.MustRun()
 }
